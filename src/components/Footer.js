0b48358d6d84a38517c5d36827417ac3b64bbf28
Steven Vi <visteven00@gmail.com>
2019-03-12 09:06:06 +1100

    initial commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 export default props =>
   <footer className="footer">
     <span className="todo-count">
-      <strong>0</strong> todos left
+      <strong>{props.remaining}</strong>
+      {props.remaining === 1 ? ' todo' : ' todos'} left
     </span>
     <ul className="filters">
       <li><Link to="/">All</Link></li>
